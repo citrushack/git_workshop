f07ef5782576fa194f4e2ea37b6ecf59cdbe78a5
Divyank Shah <divyank.shah.2016@gmail.com>
2022-11-05 11:41:49 -0700

    make a test pr

--- test_factorial.py
+++ test_factorial.py
@@ -5,11 +5,11 @@
 
 from factorial import factorial
 
-def test_factorial():
+def test_factorial_6():
     assert 6 == factorial(3)
 
-def test_factorial():
+def test_factorial_4():
     assert 24 == factorial(4)
 
-def test_factorial():
+def test_factorial_5():
     assert 120 == factorial(5)
